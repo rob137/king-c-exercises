f2cd59a61dff9b0e432d57052921608afec17ef3
rob <robertaxelkirby@gmail.com>
2020-04-10 16:41:10 +0100

    random_integers.c

---
+++ random_integers.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+  int a, b, c, d, e, f, g;
+  float h, i, j, k, l, m, n;
+  printf("%i\n%i\n%i\n%i\n%i\n%i\n%i\n\n", a, b, c, d, e, f, g);
+  printf("%f\n%f\n%f\n%f\n%f\n%f\n%f\n", h, i, j, k, l, m, n);
+  return 0;
+}
