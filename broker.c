7ff86d6f1323776e67a7346e5891646501374f0d
rob <robertaxelkirby@gmail.com>
2020-04-13 13:39:39 +0100

    "broker.c"

---
+++ broker.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(void) {
+  float trade, commission;
+
+  printf("Enter value of trade: ");
+  scanf("%d", &trade);
+
+  if (trade < 2500) {
+    commission = trade * 0.017f + 30.0f;
+  } else if (trade <= 6250) {
+    commission = trade * 0.066f + 56.0f;
+  } else if (trade <= 20000) {
+    commission = trade * 0.034f + 76.0f;
+  } else if (trade <= 50000) {
+    commission = trade * 0.022f + 100.0f;
+  } else if (trade <= 500000) {
+    commission = trade * 0.011f + 155.0f;
+  } else {
+    commission = trade * 0.009f + 255.0f;
+  }
+
+  if (trade <= 39) {
+    commission = 39.00f;
+  }
+
+  printf("Commission: %.2f\n", commission);
+
+  return 0;
+}
