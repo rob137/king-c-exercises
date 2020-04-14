0efb82d4ae288d2cb7d600997a5057f7924634fb
rob <robertaxelkirby@gmail.com>
2020-04-14 14:56:32 +0100

    "flights.c"

--- flights.c
+++ flights.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 int main(void) {
-  int inputHour, inputMins, inputTotal, dHour, dMins, aHour, aMins, time1,
-      time2, time3, time4, time5, time6, time7, time8;
-  char amPm;
+  int inputHour, inputMins, inputTotal, dHour, dMins, aHour, aMins, dMinsTens,
+      dMinsOnes, aMinsTens, aMinsOnes, time1, time2, time3, time4, time5, time6,
+      time7, time8;
 
-  printf("Enter a 24-hour time: ");
+  printf("Enter a 24-hour time using the format \"HH:MM\": ");
   scanf("%d:%d", &inputHour, &inputMins);
 
   inputTotal = inputHour * 60 + inputMins;
@@ -22,36 +22,60 @@ int main(void) {
   if (inputTotal <= 252) {
     dHour = 21;
     dMins = 45;
+    aHour = 23;
+    aMins = 58;
   } else if (inputTotal < time1 + (time2 - time1) / 2) {
     dHour = 8;
     dMins = 0;
+    aHour = 10;
+    aMins = 16;
   } else if (inputTotal < time2 + (time3 - time2) / 2) {
     dHour = 9;
     dMins = 43;
+    aHour = 11;
+    aMins = 52;
   } else if (inputTotal < time3 + (time4 - time3) / 2) {
     dHour = 11;
     dMins = 19;
+    aHour = 13;
+    aMins = 31;
   } else if (inputTotal < time4 + (time5 - time4) / 2) {
     dHour = 12;
     dMins = 47;
+    aHour = 15;
+    aMins = 0;
   } else if (inputTotal < time5 + (time6 - time5) / 2) {
     dHour = 14;
     dMins = 0;
+    aHour = 16;
+    aMins = 8;
   } else if (inputTotal < time6 + (time7 - time6) / 2) {
     dHour = 15;
     dMins = 45;
+    aHour = 17;
+    aMins = 55;
   } else if (inputTotal < time7 + (time8 - time7) / 2) {
     dHour = 19;
     dMins = 0;
+    aHour = 21;
+    aMins = 20;
   } else {
     dHour = 21;
     dMins = 45;
+    aHour = 23;
+    aMins = 58;
   }
 
-  printf("Closest departure time is %d:%d ", dHour, dMins);
-  printf("%c.m., ", amPm);
-  printf("arriving at %d:%d ", aHour, aMins);
-  printf("%c.m.\n", aHour < 12 ? "p" : "a");
+  dMinsTens = dMins / 10;
+  dMinsOnes = dMins - dMinsTens * 10;
+
+  aMinsTens = aMins / 10;
+  aMinsOnes = aMins - aMinsTens * 10;
+
+  printf("Closest departure time is %d:%d%d ", dHour, dMinsTens, dMinsOnes);
+  printf("%s.m., ", dHour < 12 ? "a" : "p");
+  printf("arriving at %d:%d%d ", aHour, aMinsTens, aMinsOnes);
+  printf("%s.m.\n", aHour < 12 ? "a" : "p");
 
   return 0;
 }
